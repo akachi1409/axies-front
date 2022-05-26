43b40c76b2b2f7e20260e1f91f49bd3eed3505e3
trusteddev1409 <trusteddev1325@gmail.com>
2022-05-27 01:10:17 +0900

    image alt fix

--- src/components/normal/helpComp/helpItem.js
+++ src/components/normal/helpComp/helpItem.js
@@ -3,7 +3,7 @@ import "./helpItem.css";
 function HelpItem(props){
     return(
         <div className="helpItem-layout">
-            <img style={{marginTop:"1em"}} src={props.Img}/>
+            <img style={{marginTop:"1em"}} alt="" src={props.Img}/>
             <h3 className="helpItem-title">{props.title}</h3>
             <p className="helpItem-text">{props.text}</p>
         </div>
