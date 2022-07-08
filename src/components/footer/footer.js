c318176a95a32570be821247af20fe5dbfc8c5a1
trusteddev1409 <trusteddev1325@gmail.com>
2022-07-08 06:29:39 -0400

    fix footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -1,91 +1,64 @@
-import React, { Component } from "react";
+import React from "react";
 
 import "./footer.css";
 import { Container, Row } from "react-bootstrap";
-// import Mono_Logo from "../../assets/footer/Logo.png";
-import Facebook from "../../assets/footer/facebook.png";
-import Twitter from "../../assets/footer/twitter.png";
-import Google from "../../assets/footer/google.png";
-import Twitch from "../../assets/footer/twitter.png";
 
-class Footer extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      data: [
-        {
-          title: "My Account",
-          text: [
-            "Authors",
-            "Collection",
-            "Author Profile",
-            "Create Collection",
-          ],
-        },
-        {
-          title: "Resources",
-          text: ["Help & Support", "Live Auctions", "Item Details", "Activity"],
-        },
-        {
-          title: "Company",
-          text: ["About Us", "Contact Us", "Our Blog", "Discover"],
-        },
-      ],
-      isMobile: false,
-    };
-  }
-  render() {
-    return (
-      <div className="footer-container">
-        <Container>
-          <section className="footer-section1">
-            <div className="footer-section1-layout">
-              <div className="footer-sectiion-icon">
-                <img
-                  alt=""
-                  src={Facebook}
-                  className="footer-sectiion-icon-round"
-                />
-              </div>
-              <div className="footer-sectiion-icon">
-                <img
-                  alt=""
-                  src={Twitter}
-                  className="footer-sectiion-icon-round"
-                />
-              </div>
-              <div className="footer-sectiion-icon">
-                <img
-                  alt=""
-                  src={Google}
-                  className="footer-sectiion-icon-round"
-                />
-              </div>
-              <div className="footer-sectiion-icon">
-                <img
-                  alt=""
-                  src={Twitch}
-                  className="footer-sectiion-icon-round"
-                />
-              </div>
-            </div>
-            <Row>
-              <p className="footer-sectiion1-text">
-                <span className="footer-sectiion1-span">CATENA</span> is a connection from the past to the present to the future
-              </p>
-            </Row>
-          </section>
-          <section className="footer-copyright">
-            <Row>
-              <p className="footer-copyright-text">
-              Copyright 2022 Catena. All Rights Reserved.
-              </p>
-            </Row>
-          </section>
-        </Container>
-      </div>
-    );
-  }
+import Twitter from "../../assets/footer/twitter.png";
+import Twitch from "../../assets/footer/twitch.png";
+import Discord from "../../assets/footer/discord.png";
+
+function Footer () {
+
+  return (
+    <div className="footer-container">
+      <Container>
+        <section className="footer-section1">
+          <div className="footer-section1-layout">
+            {/* <div className="footer-sectiion-icon">
+              <img
+                alt=""
+                src={Facebook}
+                className="footer-sectiion-icon-round"
+              />
+            </div> */}
+            <a className="footer-sectiion-icon" href="https://twitter.com/welcomecatena">
+              <img
+                alt=""
+                src={Twitter}
+                className="footer-sectiion-icon-round"
+              />
+            </a>
+            <a className="footer-sectiion-icon" href="https://community.catena.one/">
+              <img
+                alt=""
+                src={Twitch}
+                className="footer-sectiion-icon-round"
+              />
+            </a>
+            <a className="footer-sectiion-icon" href="http://bit.ly/metacommunity">
+              <img
+                alt=""
+                src={Discord}
+                className="footer-sectiion-icon-round"
+              />
+            </a>
+          </div>
+          <Row>
+            <p className="footer-sectiion1-text">
+              <span className="footer-sectiion1-span">CATENA</span> is a connection from the past to the present to the future
+            </p>
+          </Row>
+        </section>
+        <section className="footer-copyright">
+          <Row>
+            <p className="footer-copyright-text">
+            Copyright 2022 Catena. All Rights Reserved.
+            </p>
+          </Row>
+        </section>
+      </Container>
+    </div>
+  );
 }
 
 export default Footer;
